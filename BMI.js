2a92219d444b80afe15b5e92e91b56f79b2f43f3
Derek Adu <derek.o.adu91@gmail.com>
2021-05-19 16:37:35 -0400

    changed the response to determine if input is under, withing, or overweight

--- BMI.js
+++ BMI.js
@@ -2,8 +2,17 @@ function BMI(){
     var h = document.getElementById('h').value;
     var w = document.getElementById('w').value;
 
-    var bmi = w/ (h/100 * h/100);
-    var bmio = (bmi.toFixed(2))
+    var bmio = (w/(h**2))*703
 
-    document.getElementById('result').innerHTML = "Your BMI is " + bmio;
+    if(bmio < 18.5){
+        document.getElementById('result').innerHTML = `Your BMI is  ${bmio.toFixed(2)}, currently ${(18.5-bmio).toFixed(2)} underweight`;
+    }else if(bmio >= 18.5 && bmio < 24.9){
+        document.getElementById('result').innerHTML = `Your BMI is  ${bmio.toFixed(2)}, currently ${(25-bmio).toFixed(2)} within healthy weight keep it up`;
+    }else if(bmio >= 25 && bmio < 29.9){
+        document.getElementById('result').innerHTML = `Your BMI is  ${bmio.toFixed(2)}, currently ${(29.9-bmio).toFixed(2)} overweight`;
+    }else{
+        document.getElementById('result').innerHTML = `Your BMI is ${bmio.toFixed(2)}`;
+    }
+
+    
 }
